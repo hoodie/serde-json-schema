09dc277c3b13742e0764090597ccec75193883d0
Hendrik Sollich <hendrik@hoodie.de>
2019-03-03 22:53:58 +0100

    order tests

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@ pub struct Schema {
     pub schema: Url,
     pub description: String,
     // pub properties: HashMap<String, Property>,
-    // pub required: Vec<String>,
     pub dependencies: Option<HashMap<String, Vec<String>>>,
 
 
@@ -180,82 +179,83 @@ mod tests {
             assert_eq!(schema.draft_version(), Some("draft-07"))
         }
 
+        #[test]
+        fn green_door_example() {
+            let schema: Schema = serde_json::from_str(include_str!("../test/green_door.schema.json")).unwrap();
+            let json_green_door: serde_json::Value =
+                serde_json::from_str(include_str!("../test/green_door.json")).unwrap();
+            println!("{:#?}", schema);
+            schema.validate(&json_green_door).unwrap();
+        }
     }
     mod validation {
         use super::*;
-    #[test]
-    fn green_door_example() {
-        let schema: Schema = serde_json::from_str(include_str!("../test/green_door.schema.json")).unwrap();
-        let json_green_door: serde_json::Value =
-            serde_json::from_str(include_str!("../test/green_door.json")).unwrap();
-        println!("{:#?}", schema);
-        schema.validate(&json_green_door).unwrap();
+
+        #[test]
+        fn validate_wrong_numbers() {
+            let schema: Schema = serde_json::from_str(include_str!("../test/green_door.schema.json")).unwrap();
+            let json_green_door: serde_json::Value =
+                serde_json::from_str(include_str!("../test/green_door.wrong_number_types.json")).unwrap();
+            println!("{:#?}", schema);
+            assert_eq!(
+                schema.validate(&json_green_door),
+                Err(vec![String::from("expected integer found Number(1.2)")])
+                );
+        }
+
+        #[test]
+        fn validate_pure_string_object() {
+            let raw_schema = include_str!("../test/address.schema.json");
+            let schema: Schema = serde_json::from_str(raw_schema).unwrap();
+
+            let json_correct: serde_json::Value =
+                serde_json::from_str(include_str!("../test/address.json")).unwrap();
+            schema.validate(&json_correct).unwrap();
+        }
+
+        #[test]
+        fn validate_root_array() {
+            let schema: Schema = serde_json::from_str(include_str!("../test/root_array.schema.json")).unwrap();
+
+            let json_array: serde_json::Value =
+                serde_json::from_str(include_str!("../test/root_array.json")).unwrap();
+            schema.validate(&json_array).unwrap();
+        }
+
+        #[test]
+        fn validate_find_wrong_type() {
+            let raw_schema = include_str!("../test/address.schema.json");
+            let schema: Schema = serde_json::from_str(raw_schema).unwrap();
+
+            let json_wrong_type: serde_json::Value =
+                serde_json::from_str(include_str!("../test/address.wrong-type.json")).unwrap();
+            // TODO: make more concrete error type
+            assert!(schema.validate(&json_wrong_type).is_err());
+        }
+
+        #[test]
+        fn validate_find_missing_required() {
+            let raw_schema = include_str!("../test/address.schema.json");
+            let schema: Schema = serde_json::from_str(raw_schema).unwrap();
+
+            let json_missing: serde_json::Value =
+                serde_json::from_str(include_str!("../test/address.missing.json")).unwrap();
+            // TODO: make more concrete error type
+            assert!(schema.validate(&json_missing).is_err());
+        }
+
+
+        #[test]
+        fn validate_find_missing() {
+            let raw_schema = include_str!("../test/address.schema.json");
+            let schema: Schema = serde_json::from_str(raw_schema).unwrap();
+
+            let json_missing: serde_json::Value =
+                serde_json::from_str(include_str!("../test/address.missing-non-required.json")).unwrap();
+            // TODO: make more concrete error type
+            schema.validate(&json_missing).unwrap();
+        }
+
     }
-
-    #[test]
-    fn validate_wrong_numbers() {
-        let schema: Schema = serde_json::from_str(include_str!("../test/green_door.schema.json")).unwrap();
-        let json_green_door: serde_json::Value =
-            serde_json::from_str(include_str!("../test/green_door.wrong_number_types.json")).unwrap();
-        println!("{:#?}", schema);
-        assert_eq!(
-            schema.validate(&json_green_door),
-            Err(vec![String::from("expected integer found Number(1.2)")])
-            );
-    }
-
-    #[test]
-    fn validate_pure_string_object() {
-        let raw_schema = include_str!("../test/address.schema.json");
-        let schema: Schema = serde_json::from_str(raw_schema).unwrap();
-
-        let json_correct: serde_json::Value =
-            serde_json::from_str(include_str!("../test/address.json")).unwrap();
-        schema.validate(&json_correct).unwrap();
-    }
-
-    #[test]
-    fn validate_root_array() {
-        let schema: Schema = serde_json::from_str(include_str!("../test/root_array.schema.json")).unwrap();
-
-        let json_array: serde_json::Value =
-            serde_json::from_str(include_str!("../test/root_array.json")).unwrap();
-        schema.validate(&json_array).unwrap();
-    }
-
-    #[test]
-    fn validate_find_wrong_type() {
-        let raw_schema = include_str!("../test/address.schema.json");
-        let schema: Schema = serde_json::from_str(raw_schema).unwrap();
-
-        let json_wrong_type: serde_json::Value =
-            serde_json::from_str(include_str!("../test/address.wrong-type.json")).unwrap();
-        // TODO: make more concrete error type
-        assert!(schema.validate(&json_wrong_type).is_err());
-    }
-
-    #[test]
-    fn validate_find_missing_required() {
-        let raw_schema = include_str!("../test/address.schema.json");
-        let schema: Schema = serde_json::from_str(raw_schema).unwrap();
-
-        let json_missing: serde_json::Value =
-            serde_json::from_str(include_str!("../test/address.missing.json")).unwrap();
-        // TODO: make more concrete error type
-        assert!(schema.validate(&json_missing).is_err());
-    }
-
-
-    #[test]
-    fn validate_find_missing() {
-        let raw_schema = include_str!("../test/address.schema.json");
-        let schema: Schema = serde_json::from_str(raw_schema).unwrap();
-
-        let json_missing: serde_json::Value =
-            serde_json::from_str(include_str!("../test/address.missing-non-required.json")).unwrap();
-        // TODO: make more concrete error type
-        schema.validate(&json_missing).unwrap();
-    }
-
 }
 
