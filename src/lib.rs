ded356f3bbf376780194822c6f2919ae8b25e64f
Hendrik Sollich <hendrik@hoodie.de>
2019-03-03 20:30:30 +0100

    better minimal validation

--- src/lib.rs
+++ src/lib.rs
@@ -28,9 +28,9 @@ impl Schema {
             .and_then(|mut segments| segments.next())
     }
 
-    pub fn validate(&self, json: &serde_json::Value) -> bool {
+    pub fn validate(&self, json: &serde_json::Value) -> Result<(), Vec<String>> {
         match &self.specification {
-            Property::Inline(ref prop) => prop.validate(json),
+            Property::Value(ref prop) => prop.validate(json),
             Property::Ref(_) => unimplemented!()
         }
     }
@@ -40,7 +40,7 @@ impl Schema {
 #[serde(untagged)]
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Property {
-    Inline(SchemaValue),
+    Value(SchemaValue),
     Ref(RefProperty),
 }
 
@@ -60,34 +60,50 @@ pub enum SchemaValue {
 
 impl SchemaValue {
 
-    pub fn validate(&self, json: &serde_json::Value) -> bool {
+    pub fn validate(&self, json: &serde_json::Value) -> Result<(), Vec<String>> {
         use SchemaValue::*;
         use serde_json::Value;
 
         match (&self, json) {
-            (String, Value::String(_)) => true,
+            (String, Value::String(_)) => Ok(()),
             (String, unexpected_value) => {
-                eprintln!("expected string found {}", unexpected_value);
-                false
+                Err(vec![format!("expected string found {:?}", unexpected_value)])
             },
 
-            (Array{ items }, Value::Array(elems)) => elems.iter().all(|elem| items.validate(&elem)),
-            (Array{ .. }, _) => false,
+            (Array{ items }, Value::Array(elems)) => {
+                let errors: Vec<std::string::String> = elems
+                    .iter()
+                    .map(|value| items.validate(&value))
+                    .filter_map(Result::err)
+                    .flat_map(|errors| errors.into_iter())
+                    .collect();
+                if errors.is_empty() { Ok(()) } else {
+                    Err(errors)
+                }
+            },
+            (Array{ .. }, unexpected_value) => {
+                Err(vec![format!("expected array found {:?}", unexpected_value)])
+            },
 
-            (Object{ properties }, Value::Object(hash)) => {
-                hash.iter()
-                    .all(|(k, v)|
-                    properties
+            (Object{ properties }, Value::Object(object)) => {
+                let errors: Vec<std::string::String> = properties
+                    .iter()
+                    .filter_map(|(k, schema)| object
                         .get(k)
-                        .map(|s| match s {
-                            Property::Inline(schema) => schema.validate(v),
+                        .map(|v| match schema {
+                            Property::Value(schema) => schema.validate(v).err(),
                             Property::Ref(_schema) => unimplemented!(),
                         })
-                        .unwrap_or(false)
+                        .unwrap_or(Some(vec![format!("object doesn't contain {}", k)]))
                     )
+                    .flat_map(|errors| errors.into_iter())
+                    .collect();
+                if errors.is_empty() { Ok(()) } else {
+                    Err(errors)
+                }
             },
 
-            (Object{ .. }, _) => false,
+            (Object{ .. }, _) => Err(vec![format!("invalid object")]),
         }
     }
 }
@@ -132,15 +148,36 @@ mod tests {
     }
 
     #[test]
-    fn validate_string_object() {
-        let raw_json = include_str!("../test/address.json");
+    fn validate_pure_string_object() {
         let raw_schema = include_str!("../test/address.schema.json");
-
-        let json: serde_json::Value = serde_json::from_str(raw_json).unwrap();
         let schema: Schema = serde_json::from_str(raw_schema).unwrap();
 
-        println!("{:#?}{:#?}", schema, json);
-        assert!(schema.validate(&json))
+        let json_correct: serde_json::Value =
+            serde_json::from_str(include_str!("../test/address.json")).unwrap();
+        schema.validate(&json_correct).unwrap();
+    }
+
+    #[test]
+    fn validate_find_wrong_type() {
+        let raw_schema = include_str!("../test/address.schema.json");
+        let schema: Schema = serde_json::from_str(raw_schema).unwrap();
+
+        let json_wrong_type: serde_json::Value =
+            serde_json::from_str(include_str!("../test/address.wrong-type.json")).unwrap();
+        // TODO: make more concrete error type
+        assert!(schema.validate(&json_wrong_type).is_err());
+    }
+
+    #[test]
+    fn validate_find_missing() {
+        let raw_schema = include_str!("../test/address.schema.json");
+        let schema: Schema = serde_json::from_str(raw_schema).unwrap();
+
+        let json_missing: serde_json::Value =
+            serde_json::from_str(include_str!("../test/address.missing.json")).unwrap();
+        // TODO: make more concrete error type
+        assert!(schema.validate(&json_missing).is_err());
+
     }
 
 }
