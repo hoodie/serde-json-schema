d705a744fbc2d281ca988eda923862f7016b346a
Hendrik Sollich <hendrik@hoodie.de>
2019-08-29 23:03:12 +0200

    add little usage example

---
+++ examples/usage.rs
@@ -0,0 +1,15 @@
+use serde_json;
+use serde_json_schema::Schema;
+use std::convert::TryFrom;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use std::fs;
+    let schema_file = fs::read_to_string("./examples/address.schema.json")?;
+    //let address_schema = Schema::try_from(schema_file)?;
+    let address_schema = Schema::try_from(schema_file)?;
+    println!("{:#?}", address_schema);
+    println!("{}", serde_json::to_string_pretty(&address_schema)?);
+    // println!("{:#?}", address_schema.specification().unwrap());
+    // println!("{:#?}", address_schema.properties().unwrap());
+    Ok(())
+}
