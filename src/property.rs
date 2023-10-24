1529a3016d0e1fc9f7471fae2ea307798dcdb868
PWall <34860495+PWall2222@users.noreply.github.com>
2023-10-24 20:25:02 +0200

    feat: dereferncing

--- src/property.rs
+++ src/property.rs
@@ -2,9 +2,9 @@
 
 use serde::{Deserialize, Serialize};
 
-use std::collections::HashMap;
+use std::{collections::HashMap, str::Split};
 
-use crate::validation::NumberCriteria;
+use crate::{validation::NumberCriteria, Schema};
 
 /// Either a `PropertyInstance` or a reference
 #[serde(untagged)]
@@ -13,11 +13,75 @@ pub enum Property {
     Value(PropertyInstance),
     Ref(RefProperty),
 }
-/// TODO: implement dereferencing
+
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct RefProperty {
     #[serde(rename = "$ref")]
     pub reference: String,
+}
+
+#[derive(Debug)]
+enum Data<'a> {
+    Map(&'a HashMap<String, Property>),
+    Prop(&'a Property),
+    Instance(&'a PropertyInstance),
+    Schema(&'a Schema),
+}
+
+fn get_items(p: &Property) -> Option<&PropertyInstance> {
+    match p {
+        Property::Value(v) => match v {
+            PropertyInstance::Array { items } => Some(&**items),
+            _ => None,
+        },
+        _ => None,
+    }
+}
+
+fn get_properties_instance(p: &PropertyInstance) -> Option<&HashMap<String, Property>> {
+    match p {
+        PropertyInstance::Object { properties, .. } => Some(properties),
+        _ => None,
+    }
+}
+
+fn get_properties(p: &Property) -> Option<&HashMap<String, Property>> {
+    match p {
+        Property::Value(v) => get_properties_instance(v),
+        _ => None,
+    }
+}
+
+fn find_ref<'a>(mut path: Split<'a, &'a str>, mut data: Data<'a>) -> Option<Data<'a>> {
+    loop {
+        let Some(branch) = path.next() else {
+            return Some(data);
+        };
+        data = match (branch, data) {
+            ("properties", Data::Instance(v)) => Data::Map(get_properties_instance(v)?),
+            ("properties", Data::Map(v)) => Data::Map(get_properties(v.get(branch)?)?),
+            ("properties", Data::Prop(v)) => Data::Map(get_properties(&v)?),
+            ("properties", Data::Schema(v)) => Data::Map(v.properties()?),
+            ("items", Data::Prop(v)) => Data::Instance(get_items(&v)?),
+            (_, Data::Map(v)) => Data::Prop(v.get(branch)?),
+            _ => return None,
+        };
+    }
+}
+
+impl RefProperty {
+    pub fn deref<'a>(&'a self, schema: &'a Schema) -> Option<&PropertyInstance> {
+        let reference = self.reference.strip_prefix("#/")?;
+        let path = reference.split("/").into_iter();
+        match find_ref(path, Data::Schema(schema))? {
+            Data::Prop(v) => match v {
+                Property::Ref(v) => Some(v.deref(schema)?),
+                Property::Value(v) => Some(v),
+            },
+            Data::Instance(v) => Some(v),
+            _ => None,
+        }
+    }
 }
 
 /// Represents the [Instance Data Model](https://json-schema.org/latest/json-schema-core.html#rfc.section.4.2.1)
